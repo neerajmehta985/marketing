996ac92e766d42207d63186b3533a8b9995d92c8
Neeraj <neerajmehta985@gmail.com>
2024-09-27 11:44:49 +0530

    created server folder

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+
+const port = 5000
+
+
+mongoose.connect(process.env.MongoDb)
+.then(()=>{console.log('MongoDB Start');})
+.catch((e)=>{console.log(e);})
+
+app.listen(port,()=>{console.log(`Server is Running ${port}`);})
